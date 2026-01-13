b640d990ae622bcf2ae22179b4dec2fc1583b47e
Тимофей <timoha_zelenov@mail.ru>
2026-01-13 23:43:31 +0300

    first commit

---
+++ 1.js
@@ -0,0 +1,43 @@
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
+
+const release = 2007;
+alert(release);
+
+const NameJava = 'Брендан Айк';
+alert(NameJava);
+
+a = 10;
+let b = 2;
+alert(`${a + b}\n${a - b}\n${a * b}\n${a / b}`);
+
+let result = b ** 5;
+alert(result);
+
+a = 9;
+b = 2;
+result = 9 % 2;
+alert(result);
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+
+const age = (prompt('Сколько вам лет?'));
+alert(age);
+
+const User = {
+    name: 'Макс',
+    age: Number(40),
+    isAdmin: true
+}
+
+const YourName = (prompt('Как тебя зовут?'));
+alert(`Привет, ${YourName}!`);
